0944eaaa1301b477410aac2951c81b767c974b73
itskhurrams <itskhurrams@gmail.com>
2023-02-19 00:03:42 +0500

    q16

---
+++ q16.js
@@ -0,0 +1,19 @@
+var names15Q = [
+    'Abcdef',
+    'Bcdef',
+    'Cdefhi',
+    'Defgkl',
+    'Efghno',
+    'Fghiqr',
+    'Ghiktu',
+    'Hijkxw',
+    'Ijklyz',
+    'Jkliab',
+];
+for (var k = 0; k < names15Q.length; k++) {
+    if (names15Q[k] == 'Defgkl')
+        names15Q[k] = 'New Guest Name';
+    console.log(names15Q[k]);
+}
+// print statement for one who does not make it
+console.log('\r\n Mr & Mrs. Defgkl will not be able to make it.');
